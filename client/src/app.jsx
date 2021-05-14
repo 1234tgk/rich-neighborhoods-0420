8bda3ed025b18445789360a780b1df2c5dfbb61a
Jean Kang <jeankang.dev@gmail.com>
2021-05-14 14:44:58 -0400

    make components and add router

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import styles from './app.module.css';
 import Header from './components/header/header';
 import Budget from './components/budget/budget';
 import Main from './components/main/main';
 import Members from './components/members/members';
 import Footer from './components/footer/footer';
+import memberData from '../src/data/data';
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
+    <div className={styles.app}>
+      <div className={styles.header}>
         <Header />
-        <Route exact path="/"><Main /></Route>
-        <Route path="/budget"><Budget /></Route>
-        <Route path="/dashboard"><Members /></Route>
+      </div>
+      <div className={styles.container}>
+        <BrowserRouter>
+          <Switch>
+            {/* <Route exact path="/"><Main /></Route> */}
+            {/* <Route path="/budget"><Budget /></Route> */}
+            <Route path="/dashboard"><Members memberData={memberData} /></Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+      <div className={styles.footer}>
         <Footer />
-      </BrowserRouter>
+      </div>
     </div>
   );
 };
