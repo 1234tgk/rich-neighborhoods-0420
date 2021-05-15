430b09971513c7a76036b99f8e743cf4138cc537
Komogi <billy.bao11@gmail.com>
2021-05-15 02:52:22 -0400

    Improved PUT request
    PUT request now returns null and no longer creates a new Member/Transaction if corresponding id is not found

--- server/src/main/java/com/richneighborhoods0420/server/MemberController.java
+++ server/src/main/java/com/richneighborhoods0420/server/MemberController.java
@@ -25,9 +25,19 @@ public class MemberController {
         return memberRepository.save(member);
     }
     
-    @PutMapping
-    public void updateMember(@Validated @RequestBody Member member) {
-    	memberRepository.save(member);
+    @PutMapping("/{id}")
+    public Member updateMember(@PathVariable long id, @Validated @RequestBody Member newMember) {
+        Member updatedMember = memberRepository.findById(id)
+                .map(member -> {
+                    member.setName(newMember.getName());
+                    member.setStatus(newMember.getStatus());
+                    return memberRepository.save(member);
+                })
+                .orElseGet(() -> {
+                    return null;
+                });
+
+        return updatedMember;
     }
 
     // no need to delete member, but here it is anyway
