2f53972c09d1e055dcb4a3e150d56adec03c6484
Jean Kang <jeankang.dev@gmail.com>
2021-05-15 01:26:06 -0400

    correct typo in budget data

--- client/src/data/budget.js
+++ client/src/data/budget.js
@@ -1,6 +1,6 @@
 export default [
-  {"id": 1, "date": "2021-05-01", "detail": "Raspberry Pi", "Amount": 100, "Balance": 400},
-  {"id": 2, "date": "2021-05-02", "detail": "Second-hand monitor", "Amount": 150, "Balance": 250},
-  {"id": 3, "date": "2021-05-03", "detail": "Standing Banner for events", "Amount": 50, "Balance": 200},
-  {"id": 4, "date": "2021-05-04", "detail": "Flowers for graduates", "Amount": 30, "Balance": 170},
+  {"id": 1, "date": "2021-05-01", "detail": "Raspberry Pi", "amount": 100, "balance": 400},
+  {"id": 2, "date": "2021-05-02", "detail": "Second-hand monitor", "amount": 150, "balance": 250},
+  {"id": 3, "date": "2021-05-03", "detail": "Standing Banner for events", "amount": 50, "balance": 200},
+  {"id": 4, "date": "2021-05-04", "detail": "Flowers for graduates", "amount": 30, "balance": 170},
 ]
