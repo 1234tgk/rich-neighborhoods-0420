7b4b8930f046b5270e3bda48259abe13a3dc9759
Jean Kang <jeankang.dev@gmail.com>
2021-05-15 02:13:09 -0400

    add images to main

--- client/src/components/main/main.jsx
+++ client/src/components/main/main.jsx
@@ -5,6 +5,7 @@ const Main = () => {
   return (
     <div className={styles.main}>
       <h1>Main</h1>
+      <img src={process.env.PUBLIC_URL + './images/custom_mac1.PNG'} alt="" width="500px"></img>
     </div>
   );
 };
