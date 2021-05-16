dcc6a331210b5442fe6d74ad5a50507175334d58
Taehyeon Kim <1234tgk@gmail.com>
2021-05-16 00:53:40 -0400

    image fix

--- client/src/components/club/club.jsx
+++ client/src/components/club/club.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import styles from './club.module.css';
+import customLaptop1 from '../../assets/images/custom_laptop1.jpg';
+import customLaptop2 from '../../assets/images/custom_laptop2.PNG';
+import smartMirror2 from '../../assets/images/smart_mirror2.PNG';
+import raspberryPi1 from '../../assets/images/raspberry_pi1.png';
+import raspberryPi2 from '../../assets/images/raspberry_pi2.png';
+import raspberryPiTop from '../../assets/images/raspberry_pi_top.png';
 
 const Club = () => {
   return (
@@ -7,18 +13,18 @@ const Club = () => {
       <section className={styles.dream}>
         <h3 className={styles.title}>What do we want to do?</h3>
         <section className={styles.images}>
-          <img src={process.env.PUBLIC_URL + './images/custom_laptop1.jpg'} alt=""></img>
-          <img src={process.env.PUBLIC_URL + './images/custom_laptop2.PNG'} alt=""></img>
-          <img src={process.env.PUBLIC_URL + './images/smart_mirror2.png'} alt=""></img>
+          <img src={customLaptop1} alt=""></img>
+          <img src={customLaptop2} alt=""></img>
+          <img src={smartMirror2} alt=""></img>
         </section>
       </section>
 
       <section className={styles.reality}>
         <h3 className={styles.title}>What do we need?</h3>
         <div className={styles.images}>
-          <img src={process.env.PUBLIC_URL + './images/raspberry_pi1.png'} alt=""></img>
-          <img src={process.env.PUBLIC_URL + './images/raspberry_pi2.png'} alt=""></img>
-          <img src={process.env.PUBLIC_URL + './images/raspberry_pi_top.png'} alt=""></img>
+          <img src={raspberryPi1} alt=""></img>
+          <img src={raspberryPi2} alt=""></img>
+          <img src={raspberryPiTop} alt=""></img>
         </div>
       </section>
     </div>
