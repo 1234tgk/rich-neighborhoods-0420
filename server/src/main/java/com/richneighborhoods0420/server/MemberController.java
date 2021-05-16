a3c61c20a9bf21e684f4ff5a01138b849ad86e31
Komogi <billy.bao11@gmail.com>
2021-05-15 22:03:13 -0400

    Creating and Updating a Member creates new Transaction
    also added new idLimit as well as membership fee for Members

--- server/src/main/java/com/richneighborhoods0420/server/MemberController.java
+++ server/src/main/java/com/richneighborhoods0420/server/MemberController.java
@@ -13,6 +13,12 @@ public class MemberController {
 
     @Autowired
     private MemberRepository memberRepository;
+    
+    @Autowired
+    private TransactionRepository transactionRepository;
+    
+    public long idLimit = 1000;
+    public double membershipFee = 20.0;
 
     @GetMapping
     public Iterable<Member> findAllMembers() { return memberRepository.findAll(); }
@@ -24,6 +30,12 @@ public class MemberController {
 
     @PostMapping
     public Member createMember(@Validated @RequestBody Member member) {
+    	// update transaction list with member's payments
+        Transaction transaction = new Transaction(member.getId() + idLimit, // Arbitrary Number, max before id's get overwritten
+        										LocalDate.now().toString(),
+        										"Membership Fee for " + member.getName(), 
+        										membershipFee); // Monthly membership fee, MAGIC NUMBER
+        transactionRepository.save(transaction);
         return memberRepository.save(member);
     }
  
@@ -32,6 +44,13 @@ public class MemberController {
         Member updatedMember = memberRepository.findById(id)
                 .map(member -> {
                     member.setExpiryDate(LocalDate.now().plusMonths(1).toString());
+                    // update transaction list with member's payments
+                    Transaction transaction = new Transaction(member.getId() + idLimit, // Arbitrary Number, max before id's get overwritten
+                    										LocalDate.now().toString(),
+                    										"Membership Fee for " + member.getName(), 
+                    										membershipFee); // Monthly membership fee, MAGIC NUMBER
+                    transactionRepository.save(transaction);
+                    
                     return memberRepository.save(member);
                 })
                 .orElseGet(() -> {
